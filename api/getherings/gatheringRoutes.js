80a86759774e9f7304221252f002dfaf7b6a4fb6
alsharhanaisha <alsharhanaisha@gmail.com>
2022-03-21 12:14:31 +0300

    add main router

--- api/getherings/gatheringRoutes.js
+++ api/getherings/gatheringRoutes.js
@@ -1,9 +1,36 @@
 const express = require("express");
-const { fetchGatherings } = require("./gatheringControllers");
+const passport = require("passport");
+const {
+  fetchGatherings,
+  createGathering,
+  updateGathering,
+  deleteGathering,
+} = require("./gatheringControllers");
+const upload = require("../../middleware/multer");
 
 const router = express.Router();
 
+// fetch all gatherings
 router.get("/gatherings", fetchGatherings);
-router.post("/gatherings", signup);
 
+// create a gathering
+router.post(
+  "/gatherings",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("image"),
+  createGathering
+);
+
+// update a gathering
+router.put(
+  "/gatherings",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("image"),
+  updateGathering
+);
+
+// delete a gathering
+router.delete("/:gatheringId", deleteGathering);
+
+// export gathering router
 module.exports = router;
