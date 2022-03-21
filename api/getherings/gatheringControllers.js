85670f937ccaa6a5362a6b73a52c1389882300ad
alsharhanaisha <alsharhanaisha@gmail.com>
2022-03-21 20:12:38 +0300

    add gathering, items, locations

--- api/getherings/gatheringControllers.js
+++ api/getherings/gatheringControllers.js
@@ -1,9 +1,26 @@
 const Gathering = require("../../models/Gathering");
+const Location = require("../../models/Location");
 
 exports.fetchGatherings = async (req, res, next) => {
   try {
-    const gatherings = await Gathering.find();
+    const gatherings = await Gathering.find()
+      .populate("location")
+      .populate("guests")
+      .populate("items");
     return res.json(gatherings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.fetchSingleGathering = async (req, res, next) => {
+  try {
+    const { gatheringId } = req.params;
+    const gathering = await Gathering.findById(gatheringId)
+      .populate("location")
+      .populate("guests")
+      .populate("items");
+    return res.json(gathering);
   } catch (error) {
     next(error);
   }
@@ -12,27 +29,21 @@ exports.fetchGatherings = async (req, res, next) => {
 exports.createGathering = async (req, res, next) => {
   try {
     req.body.host = req.user._id;
+
     if (req.file) {
       req.body.image = `/${req.file.path}`;
       req.body.image = req.body.image.replace("\\", "/");
     }
-    const newGathering = await Gathering.create(req.body);
+    const newGathering = await Gathering.create(req.body)
+      .populate("location")
+      .populate("guests")
+      .populate("items");
+
     return res.status(201).json(newGathering);
   } catch (error) {
     next(error);
   }
 };
-
-// title: {
-//     type: String,
-//     required: true,
-//   },
-//   host: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-//   date: { type: String },
-//   time: { type: String },
-//   guests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-//   location: { type: mongoose.Schema.Types.ObjectId, ref: "Location" },
-//   items: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
 
 exports.updateGathering = async (req, res, next) => {
   try {
--- api/mainRouter.js
+++ api/mainRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const userRoutes = require("./users/userRoutes");
 const gatheringRoutes = require("./getherings/gatheringRoutes");
+const locationRoutes = require("./locations/locationRoutes");
+const itemRoutes = require("./items/ItemRoutes");
 const router = express.Router();
 
 router.use("/authenticate", userRoutes);
 router.use("/gatherings", gatheringRoutes);
-// router.use("/locations", locationRoutes)
-// router.use("/items", itemRoutes)
+router.use("/locations", locationRoutes);
+router.use("/items", itemRoutes);
 
 module.exports = router;
